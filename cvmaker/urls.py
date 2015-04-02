dc98a6af5f1b16e0224265da3af4ddfddde41df0
Max Sveshnikov <msveshnikov@thumbtack.net>
2015-04-02 16:42:05 +0600

    first template

--- cvmaker/urls.py
+++ cvmaker/urls.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
                        # Examples:
-                       # url(r'^$', 'cvmaker.views.home', name='home'),
+                       url(r'^$', 'cvmaker.views.home', name='home'),
                        # url(r'^blog/', include('blog.urls')),
-
                        url(r'^admin/', include(admin.site.urls)),
 )
